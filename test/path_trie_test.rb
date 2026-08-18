1a7427064b1fb8205ea1aa377df28b8246d0e117
Alexey Tomilov <atomilov25@gmail.com>
2026-08-18 18:28:08 +0500

    :cop:

--- test/path_trie_test.rb
+++ test/path_trie_test.rb
@@ -10,31 +10,31 @@ class PathTrieTest < Minitest::Test
 
   def test_exact_path_matches
     trie = Dry::Validation::Rust::PathTrie.new
-    trie.add([:profile, :email])
-    trie.add([:profile, :name])
+    trie.add(%i[profile email])
+    trie.add(%i[profile name])
 
-    assert trie.prefix?([:profile, :email])
+    assert trie.prefix?(%i[profile email])
   end
 
   def test_path_with_an_error_prefix_matches
     trie = Dry::Validation::Rust::PathTrie.new
     trie.add([:profile])
 
-    assert trie.prefix?([:profile, :email])
+    assert trie.prefix?(%i[profile email])
   end
 
   def test_path_that_is_a_prefix_of_a_stored_error_matches
     trie = Dry::Validation::Rust::PathTrie.new
-    trie.add([:profile, :email])
+    trie.add(%i[profile email])
 
     assert trie.prefix?([:profile])
   end
 
   def test_non_matching_path_does_not_match
     trie = Dry::Validation::Rust::PathTrie.new
-    trie.add([:profile, :email])
+    trie.add(%i[profile email])
 
-    refute trie.prefix?([:profile, :name])
+    refute trie.prefix?(%i[profile name])
   end
 
   def test_array_indexes_are_path_parts
@@ -47,18 +47,18 @@ class PathTrieTest < Minitest::Test
 
   def test_freeze_prevents_paths_from_being_added
     trie = Dry::Validation::Rust::PathTrie.new
-    trie.add([:profile, :email])
+    trie.add(%i[profile email])
     trie.freeze
 
     assert_predicate trie, :frozen?
-    assert_raises(FrozenError) { trie.add([:profile, :name]) }
+    assert_raises(FrozenError) { trie.add(%i[profile name]) }
   end
 
   def test_equal_tries_compare_by_paths
     left = Dry::Validation::Rust::PathTrie.new
     right = Dry::Validation::Rust::PathTrie.new
-    left.add([:profile, :email])
-    right.add([:profile, :email])
+    left.add(%i[profile email])
+    right.add(%i[profile email])
 
     assert_equal left, right
     refute_equal left, Object.new
