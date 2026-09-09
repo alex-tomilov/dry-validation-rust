da5374d76f98642541c5f33bc1d555d5935dc5ad
Alexey Tomilov <atomilov25@gmail.com>
2026-09-09 19:49:06 +0500

    test(api): cover json schema public methods

--- test/api_test.rb
+++ test/api_test.rb
@@ -8,12 +8,12 @@ class ApiTest < Minitest::Test
   CONCURRENCY_CALLS_PER_THREAD = 1_000
   SIDE_BY_SIDE_PUBLIC_API = {
     Dry::Validation::Rust::Contract => {
-      class: %i[build config import_predicates_as_macros inherited json macro_registry on_validate option option_definitions own_rules params register_macro rule rules schema schema_definition],
+      class: %i[build json_schema config import_predicates_as_macros inherited json macro_registry on_validate option option_definitions own_rules params register_macro rule rules schema schema_definition],
       instance: %i[[] call call_json default_context inspect macro_registered? resolve_macro]
     },
     Dry::Validation::Rust::Schema => {
       class: %i[JSON Params define],
-      instance: %i[[] call call_json engine fields inspect key_paths mode]
+      instance: %i[[] call has_ruby_predicates call_json engine fields inspect key_paths mode]
     },
     Dry::Validation::Rust::Contract::Result => {
       class: [],
